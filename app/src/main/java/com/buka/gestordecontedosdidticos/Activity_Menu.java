430258912006788f5d2182078d4c2ecd8bf4d955
Augusto <walsonmorais@gmail.com>
2019-03-28 15:55:19 +0100

    More actualisation from GCD...

--- app/src/main/java/com/buka/gestordecontedosdidticos/Activity_Menu.java
+++ app/src/main/java/com/buka/gestordecontedosdidticos/Activity_Menu.java
@@ -3,39 +3,81 @@ package com.buka.gestordecontedosdidticos;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.buka.gestordecontedosdidticos.fragments.Fragment_Home;
+import com.buka.gestordecontedosdidticos.fragments.Fragment_Profile;
 
 public class Activity_Menu extends AppCompatActivity {
 
+    Toolbar toolbar;
+
+    BottomNavigationView bottomNavigationView;
+    FrameLayout fragment_container;
+    Fragment selectedFragment = null;
 
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        fragment_container = findViewById(R.id.fragment_container);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    selectedFragment = new Fragment_Home();
 
-                    return true;
+                    break;
                 case R.id.navigation_add_data:
+                    Toast.makeText(Activity_Menu.this, "Add Data", Toast.LENGTH_SHORT).show();
 
-                    return true;
+                    break;
                 case R.id.navigation_user_profile:
-                    return true;
+
+                   /* SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                    editor.putString("profileid", FirebaseAuth.getInstance.getCurrentUser.getUid());
+                    editor.apply();*/
+
+                    selectedFragment = new Fragment_Profile();
+
+                    break;
             }
-            return false;
+
+            if (selectedFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-
-    }
 
 }
